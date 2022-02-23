d734bfe9e249d767364af77f45b5e683635b17e5
LiudmylaBolotova <89790016+LiudmylaBolotova@users.noreply.github.com>
2022-02-23 19:24:53 +0200

    Добавляет js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+let timerId = null;
 
+buttonStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  if (timerId) {
+    buttonStart.setAttribute('disabled', true);
+  }
+});
+
+buttonStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  if (timerId) {
+    buttonStart.removeAttribute('disabled');
+  }
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
